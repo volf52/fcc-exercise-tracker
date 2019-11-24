350d82f6635d31266bd1ff2cf31aca7c4d0735dd
Glitch (goldenrod-deer) <none>
2019-11-24 14:39:43 +0000

    👫💈 Checkpoint ./routes.js:7540316/453

--- routes.js
+++ routes.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
-const User = require("./models.js")
+const models = require("./models.js")
+
+const {User} = models;
 
 const router = express.Router();
 
 router.post("/new-user", (req, resp) => {
   const {username } = req.body;
-  const us  = new User({username})
-  return resp.status(200).json(us)
+  const user = User.findOne({username});
+  if(user){
+    return resp.status(401).json({error: "User already exists"})
+  }
+  return User.create({username}).then(user => {
+    return resp.status(200).json(user)
+  }).catch(error => {
+    console.error(error)
+    return resp.status(400).json({error})
+  })
 })
 
 router.get("/echo", (req, resp) => {
