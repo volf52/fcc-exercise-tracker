8605814628bf5d6b960f26855b760e177e14be63
Glitch (goldenrod-deer) <none>
2019-11-24 15:26:43 +0000

    😅🕴 Checkpoint ./routes.js:7540316/1018

--- routes.js
+++ routes.js
@@ -6,22 +6,27 @@ const {User} = models;
 
 const router = express.Router();
 
-router.post("/new-user", async (req, resp) => {
+router.post("/new-user", (req, resp) => {
   const {username } = req.body;
-  const user = User.findOne({username});
-  if(user){
-    return resp.status(401).json({error: "User already exists"})
-  }
-  return User.create({username}).then(user => {
-    return resp.status(200).json(user)
-  }).catch(error => {
-    console.error(error)
-    return resp.status(400).json({error})
+  User.findOne({username}).then(user => {
+    if(user){
+      console.error(user)
+      return resp.status(401).json({error: "User already exists"})
+    }
+    return User.create({username}).then(user => {
+      return resp.status(200).json({username, _id: user._id})
+    }).catch(error => {
+      console.error(error)
+      return resp.status(400).json({error})
+    })
+  }).catch(err => {
+    console.error(err)
+    return resp.status(500).json({err})
   })
 })
 
-router.get("/echo", (req, resp) => {
-  resp.json({echo: "hi"})
+router.get("/users", (req, resp) => {
+  User
 })
 
 module.exports = router;
